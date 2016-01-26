768c45f1d4ba70364d642fc5dbdb741e4bab5d8f
Leejrchau <891998346@qq.com>
2016-01-26 15:27:59 +0800

    Initial Commit

---
+++ CustomNavigationController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomNavigationController
+//
+//  Created by apple on 16/1/26.
+//  Copyright © 2016年 lizhichao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
